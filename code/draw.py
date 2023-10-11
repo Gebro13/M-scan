953cd24b8e36f6d98a4ab9e5032a7abf59545593
gebro13 <1667941288@qq.com>
2023-10-11 02:57:41 +0000

    delete some note

--- code/draw.py
+++ code/draw.py
@@ -4,6 +4,7 @@
 # # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 # import numpy as np
 # import openai
+# xxx
 
 
 import matplotlib.pyplot as plt
@@ -11,42 +12,6 @@ import matplotlib.gridspec as gridspec
 import numpy as np
 from scipy.stats import t
 
-# aliccp_auc = [0.66,0.6714,0.6631,0.6537]
-# aliccp_c = [0.0001,0.001,0.01,0.1]
-
-# cloud_auc = [0.7591,0.7608,0.7595,0.7586]
-# cloud_c = [0.01,0.1,1,10]
-
-# plt.rcParams['font.size'] = 15
-
-# fig = plt.figure(figsize=(8, 4))
-# gs = gridspec.GridSpec(1, 2)
-# print(chr(945))
-# # 绘制第一个子图
-# ax1 = fig.add_subplot(gs[0])
-# ax1.bar([1,3,5,7],aliccp_auc,color = '#FFD9D9')  # 绘制第一个图的数据
-# ax1.set_xlabel(chr(945)+'(when c = 18)')
-# ax1.set_ylabel('AUC')
-# ax1.set_title('Aliccp')
-# ax1.set_xticks(np.linspace(1,7,4))
-# ax1.set_xticklabels(['0.0001','0.001','0.01','0.1'])
-# ax1.set_ylim(0.65,0.675)
-# ax1.set_yticks(np.linspace(0.65,0.675,6))
-# # 创建第二个图
-# ax2 = fig.add_subplot(gs[1])
-# ax2.bar([1,3,5,7],cloud_auc,color = '#FFD9D9')  # 绘制第一个图的数据
-# ax2.set_xlabel(chr(945)+'(when c = 1)')
-# ax2.set_ylabel('AUC')
-# ax2.set_title('Cloud')
-# ax2.set_xticks(np.linspace(1,7,4))
-# ax2.set_xticklabels(['0.01','0.1','1','10'])
-# ax2.set_ylim(0.755,0.765)
-# ax2.set_yticks(np.linspace(0.755,0.765,6))
-
-# plt.tight_layout()
-
-# plt.show()
-# plt.savefig('hyperparameter_alpha.pdf')
 
 
 aliccp_auc = [0.6605,0.6674,0.6693,0.6714,0.6694]
@@ -59,7 +24,7 @@ plt.rcParams['font.size'] = 15
 
 fig = plt.figure(figsize=(8, 4))
 gs = gridspec.GridSpec(1, 2)
-
+  
 # draw the first sub-graph
 ax1 = fig.add_subplot(gs[0])
 ax1.plot(aliccp_c,aliccp_auc,color ='#90baf4')  # feed data in the first graph
